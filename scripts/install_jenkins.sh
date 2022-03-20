34229a36cf67aeaa08847a1b3e1b1a0aa8959027
Gitesh Wagadre <Giteshwagadre@gmail.com>
2022-03-20 13:09:49 +0530

    new changes 2

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -3,8 +3,10 @@
 # this script is only tested on ubuntu focal 20.04 (LTS)
 
 # install docker
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get update
-sudo apt-get install -y 
+sudo apt-get install -y \ 
     apt-transport-https \
     ca-certificates \
     curl \
